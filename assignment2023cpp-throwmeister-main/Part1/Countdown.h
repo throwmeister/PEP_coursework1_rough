8b42a13251461b85b869e6179cd9a2804a4757bf
AlexWickman <alexander.wickman@kcl.ac.uk>
2023-10-27 16:03:42 +0100

    Part implementation of RPN builder loop

--- assignment2023cpp-throwmeister-main/Part1/Countdown.h
+++ assignment2023cpp-throwmeister-main/Part1/Countdown.h
@@ -45,7 +45,7 @@ public:
 
 void createNumPermutationsRecursion(std::vector<std::vector<std::string>> &finalPermutations, const std::vector<std::string> &vals,
  std::vector<std::string> &permutation, std::vector<bool> &distinctVal, const int targetSize){
-
+    // Checks for uniqueness in vals
     if (permutation.size() == targetSize){
         // Push finished permutation onto vector
         finalPermutations.push_back(permutation);
@@ -61,28 +61,19 @@ void createNumPermutationsRecursion(std::vector<std::vector<std::string>> &final
             distinctVal[i] = false;
             permutation.pop_back();
         }
-        
     }
 }
 
 void createOperatorPermutationsRecursion(std::vector<std::vector<std::string>> &finalPermutations, const std::vector<std::string> &vals,
  std::vector<std::string> &permutation, const int targetSize){
-    
+    // Does not check for uniqueness in vals
     if (permutation.size() == targetSize){
         // Push finished permutation onto vector
         finalPermutations.push_back(permutation);
         return;
     }
 
-    if (permutation.size() == 4){
-        for (int i = 0; i < vals.size(); i++){
-            permutation.push_back(vals[i]);
-            createOperatorPermutationsRecursion(finalPermutations, vals, permutation, targetSize);
-            permutation.pop_back();
-        }
-    }
     for (int i = 0; i < vals.size(); i++){
-        // Checker for distinct value
         // recursion loop
         // {x - / *}
         permutation.push_back(vals[i]);
@@ -109,28 +100,57 @@ std::vector<std::vector<std::string>> permuteOperators(const std::vector<std::st
 }
 
 
+
+
 CountdownSolution solveCountdownProblem(std::vector<int> numbers, const int targetNum){
     // bruteforce: build every possible rpn expression until the target number is reached or the closest#
     // Idea: when bulding the rpn string, check if + and * expressions do the same thing: prevent repeat evalutations
 
     
-    double closestTarget = 0;
-    std::vector<std::string> operators = { "+", "-", "*", "/" };
+    int closestTarget = 0;
+    const std::vector<std::string> operators = { "+", "-", "*", "/" };
     
-    std::vector<std::string> strNums;
+    // temp holder
+    std::vector<std::string> tmpStrNums;
 
     for (int num: numbers){
-        strNums.push_back(intToString(num));
+        tmpStrNums.push_back(intToString(num));
     }
 
-    std::vector<std::vector<std::string>> finalSolution = permuteString(strNums, 3);
+    const std::vector<std::string> strNums = tmpStrNums;
+    /*
+    std::vector<std::vector<std::string>> finalSolution = permuteString(strNums, 5);
 
-    std::vector<std::vector<std::string>> finalOps = permuteOperators(operators, 3);
+    std::vector<std::vector<std::string>> finalOps = permuteOperators(operators, 5);
 
-    
     for (const std::vector<std::string>& expression : finalSolution) {
-        // std::cout << expression << std::endl;
+        for (const std::string &exp: expression){
+            std::cout << exp << " ";
+        }
+        std::cout << "\n";
     }
+    */
+
+    // 5 Layers 1-5
+    for (int i = 1; i < 3; i++){
+        std::vector<std::vector<std::string>> finalNums = permuteString(strNums, i+1);
+        std::vector<std::vector<std::string>> finalOps = permuteOperators(operators, i);
+        std::vector<std::vector<std::string>> finalRPNs;
+
+        for(auto &expression: finalNums){
+            std::vector<std::string> buildRPN;
+
+            for (int _ = 0; _<2; _++){
+                buildRPN.push_back(expression[0]);
+                expression.pop_back();
+            }
+            
+
+        }
+
+    }
+    
+    
 
     CountdownSolution c;
     return c;
@@ -147,7 +167,7 @@ void pop_2_elements(std::vector<double> &stack, double* n1, double* n2){
 };
 
 
-double evaluateCountdown(std::string rnp_string){
+double evaluateCountdown(const std::string &rnp_string){
     // rnp_string is a reverse-polish notation expression
     std::vector<double> stack(6);
 
@@ -184,7 +204,6 @@ double evaluateCountdown(std::string rnp_string){
             numstream << val;
             double val_num;
             numstream >> val_num;
-
             stack.push_back(val_num);
         }
     }
